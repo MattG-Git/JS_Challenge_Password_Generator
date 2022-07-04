3e6e09fc1d1e80dd5a883f4d94202a7103b42a4b
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2022-07-04 16:25:11 -0700

    finished js code and added readme file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# JS_Password_Generator
+This is a password generator built using Vanilla JavaScript, HTML and CSS. 
+
+I updated the JavaScript code to meet the following acceptance criteria:
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+Please see [live page here](https://mattg-git.github.io/JS_Challenge_Password_Generator/) and screenshot of project below.
+
+![password generator](screencapture-mattg-git-github-io-JS-Challenge-Password-Generator-2022-07-04-16_23_05.png)
+
--- script.js
+++ script.js
@@ -2,56 +2,91 @@
 var generateBtn = document.querySelector("#generate");
 //This stores the user's password length input
 var l;
+// these are the possible strings used in userChoice variable
 var upperAlph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerAlph = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
-var symbols = "!#$%&*@";
+var symbols = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
 
 
 
-var generatePassword = function(l, isUpperCase, isLowerCase, includeNumbers, specialCharacters){
+var generatePassword = function() {
+  //generated final password
+  var pw = ""; 
+  //iterate off of user choices
+  var userChoice = ""; 
 
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  //this function gets the character length
-  var passwordLength = function(l) {
+    //this function gets the character length
+    var passwordLength = function() {
     //pops up window with initial prompt for character length
     l = window.prompt("Please choose a length for your password between 8 and 128 characters", "Enter a number");
     //this changed input to a number
     l = parseInt(l);
     //if else statement makes sure input is a number between 8 and 128
     if (l < 8 || l > 128){
-    alert("Error wrong character amount");
-    passwordLength();  
+      alert("Error: Your selection needs to be between 8 and 128 characters long");
+      passwordLength();  
     //using parseInt above, changes input to a number, this line checks if it is NaN and gives an error
   } else if (isNaN(l)){
-    alert("Not a number!");
-    passwordLength(); 
+      alert("Please choose a valid number");
+      passwordLength(); 
   } else {
-    console.log(l)
+      console.log(l)
     return l; 
-  } 
+    }
+  };
+  passwordLength();
+console.log(l);
+    //This window.confirm are the propts that equate to true or false with only "Ok" and "cancel" as the input options
+  var isUpperCase = window.confirm ("Do you want your password to have uppercase letters?");
+  console.log(isUpperCase);
+
+  if (isUpperCase) {
+    userChoice += upperAlph;
+  }
+
+  var isLowerCase = window.confirm ("Do you want your password to have lowercase letters?");
+  console.log(isLowerCase);
+
+  if (isLowerCase) {
+    userChoice += lowerAlph;
+  }
+
+  var includeNumbers = window.confirm ("Do you want your password to include numbers?");
+  console.log(includeNumbers);
+
+  if (includeNumbers) {
+    userChoice += numbers;
+  }
+
+  var specialCharacters = window.confirm ("Do you you want your password to include special characters?");
+  console.log(specialCharacters);
+
+  if (specialCharacters) {
+    userChoice += symbols;
+  }
+
+  console.log([userChoice, l]);
+
+  //for loop iterates beginning at 0 and adds random character to password string up until length of l
+  for (var i = 0; i < l; i++) {
+    pw += userChoice [Math.floor(Math.random() * userChoice.length)]
+  }
+
+  console.log(pw);
+  return pw;
 };
-passwordLength();
 
-var isUpperCase = window.confirm ("Do you want your password to have uppercase letters?");
-console.log(isUpperCase);
 
-var isLowerCase = window.confirm ("Do you want your password to have lowercase letters?");
-console.log(isLowerCase);
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-var includeNumbers = window.confirm ("Do you want your password to include numbers?");
-console.log(includeNumbers);
-
-var specialCharacters = window.confirm ("Do you you want your password to include special characters?");
-console.log(specialCharacters);
-
-passwordText.value = password;
+  passwordText.value = password;
 };
+
+
 
 
 // Add event listener to generate button
